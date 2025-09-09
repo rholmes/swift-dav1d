33b4b251d541ae5a669afe80d253b09a9c49795a
Ryan Holmes <ryan.holmes@airbnb.com>
2025-09-08 18:11:34 -0700

    Clean up target and module structure

--- Package.swift
+++ Package.swift
@@ -7,24 +7,15 @@ let package = Package(
   name: "swift-dav1d",
   platforms: [.iOS(.v13), .macOS(.v12), .macCatalyst(.v14), .tvOS(.v13)],
   products: [
-    .library(name: "dav1d", targets: ["cdav1d"]),
+    .library(name: "dav1d", targets: ["dav1d"]),
   ],
   targets: [
-    // Binary to link
     .binaryTarget(
-      name: "dav1dBinary",
-      path: "Sources/dav1d.xcframework"
-    ),
-    // C shim that vends headers + module, and depends on the binary
+      name: "dav1dBinary", path: "Sources/dav1d.xcframework"),
     .target(
-      name: "cdav1d",
+      name: "dav1d",
       dependencies: ["dav1dBinary"],
-      path: "Sources/cdav1d",
-      publicHeadersPath: "include",
-      cSettings: [
-        // If you ever need extra search paths, add them here
-        // .headerSearchPath("include")
-      ]
+      publicHeadersPath: "include"
     ),
   ]
 )
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # swift-dav1d
-Prebuilt ldav1d xcframework for iOS, macOS, Mac Catalyst and tvOS
+Prebuilt dav1d xcframework for iOS, macOS, Mac Catalyst and tvOS
