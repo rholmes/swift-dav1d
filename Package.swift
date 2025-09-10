13805915914db8399a5f06b4dee979ca45802ad6
Ryan Holmes <ryan.holmes@airbnb.com>
2025-09-09 22:20:35 -0700

    Rename C shim target

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
   name: "swift-dav1d",
   platforms: [.iOS(.v13), .macOS(.v12), .macCatalyst(.v14), .tvOS(.v13)],
   products: [
-    .library(name: "dav1d", targets: ["ClibDav1d"]),
+    .library(name: "dav1d", targets: ["Cdav1d"]),
   ],
   targets: [
     .binaryTarget(
       name: "dav1dBinary",
       path: "Sources/dav1d.xcframework"),
     .target(
-      name: "ClibDav1d",
+      name: "Cdav1d",
       dependencies: ["dav1dBinary"],
       publicHeadersPath: "include"
     ),
